698a01577f3c81794fa6b09496211904f3d2a3b5
Katalyst99 <katleho333@gmail.com>
2024-05-15 11:07:21 -0700

    Second update to base

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,19 @@ from datetime import datetime
 
 class BaseModel:
     """The class BaseModel defined"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization of Attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "__class__":
+                    continue
+                setattr(self, key, val)
 
     def __str__(self):
         """Returns the string representation"""
