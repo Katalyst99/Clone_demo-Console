5036f8fa1146987cc2cf37b36c909d7da213c5f9
Katalyst99 <katleho333@gmail.com>
2024-05-17 12:16:23 -0700

    create instance

--- console.py
+++ console.py
@@ -2,10 +2,29 @@
 """This is the module for the command interpreter entry point"""
 
 import cmd
+from models import storage
+from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
     """The class for the console defined"""
     prompt = '(hbnb) '
+    c_cls = ['BaseModel']
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel, nd prints the id."""
+        if line == '':
+            print('** class name missing **')
+        elif line not in self.c_cls:
+            print("** class doesn't exist **")
+        else:
+            c_name = eval(line)
+            instance = c_name()
+            instance.save()
+            print(instance.id)
+
+    def do_show(self, line):
+        """Prints string rep of instance based on name and id"""
 
     def do_quit(self, line):
         """Quit command to exit the program"""
